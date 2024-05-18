8aeaeb5099566bb3aecf34d19e11371d0b0a954c
chouchoufay <fay4801@gmail.com>
2024-05-18 11:45:57 +0300

    MyProgress, added heightTracker, weightTracker

---
+++ Assets/Scenes/MyProgress/heightTracker.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using System.Collections.Generic;
+
+public class UserInputHandler : MonoBehaviour
+{
+    public TMP_InputField userInputField;
+    public TextMeshProUGUI displayText;
+    public Button saveButton;
+
+    // Δημιουργία μιας δομής για την αποθήκευση ύψους και ημερομηνίας
+    [Serializable]
+    public struct HeightEntry
+    {
+        public float height;
+        public DateTime date;
+
+        public HeightEntry(float height, DateTime date)
+        {
+            this.height = height;
+            this.date = date;
+        }
+    }
+
+    // Λίστα για την αποθήκευση των καταχωρήσεων ύψους
+    public List<HeightEntry> heightEntries = new List<HeightEntry>();
+
+    void Start()
+    {
+        // Προσθήκη ακροατή στο πεδίο εισαγωγής για να ανιχνεύει τις αλλαγές
+        userInputField.onEndEdit.AddListener(HandleUserInput);
+
+        // Προσθήκη ακροατή στο κουμπί "save" για να ανιχνεύει το πάτημα του κουμπιού
+        saveButton.onClick.AddListener(SaveHeightEntry);
+    }
+
+    void HandleUserInput(string input)
+    {
+        // Εμφανίζει την εισαγωγή του χρήστη στο UI
+        displayText.text = input;
+
+        // Άλλες ενέργειες με την εισαγωγή του χρήστη
+        Debug.Log("User input: " + input);
+    }
+
+    void SaveHeightEntry()
+    {
+        // Παίρνουμε το ύψος από το πεδίο εισαγωγής
+        if (float.TryParse(userInputField.text, out float height))
+        {
+            // Παίρνουμε τη σημερινή ημερομηνία
+            DateTime today = DateTime.Today;
+
+            // Δημιουργία μιας νέας καταχώρησης ύψους με τη σημερινή ημερομηνία
+            HeightEntry newEntry = new HeightEntry(height, today);
+
+            // Προσθήκη της καταχώρησης στη λίστα
+            heightEntries.Add(newEntry);
+
+            // Εμφανίζουμε μόνο το ύψος
+            displayText.text = height.ToString();
+
+            // Άλλες ενέργειες με την καταχώρηση
+            Debug.Log($"Saved height: {height} cm on {today.ToString("dd/MM/yyyy")}");
+        }
+        else
+        {
+            // Εμφανίζουμε μήνυμα λάθους αν η είσοδος δεν είναι έγκυρος αριθμός
+            displayText.text = "Invalid input. Please enter a valid number.";
+            Debug.Log("Invalid input. Please enter a valid number.");
+        }
+    }
+}
---
+++ Assets/Scenes/MyProgress/weightTracker.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using System.Collections.Generic;
+
+public class WeightTracker : MonoBehaviour
+{
+    public TMP_InputField userInputField;
+    public TextMeshProUGUI displayText;
+    public Button saveButton;
+
+    // Δημιουργία μιας δομής για την αποθήκευση βάρους και ημερομηνίας
+    [Serializable]
+    public struct WeightEntry
+    {
+        public float weight;
+        public DateTime date;
+
+        public WeightEntry(float weight, DateTime date)
+        {
+            this.weight = weight;
+            this.date = date;
+        }
+    }
+
+    // Λίστα για την αποθήκευση των καταχωρήσεων βάρους
+    public List<WeightEntry> weightEntries = new List<WeightEntry>();
+
+    void Start()
+    {
+        // Προσθήκη ακροατή στο πεδίο εισαγωγής για να ανιχνεύει τις αλλαγές
+        userInputField.onEndEdit.AddListener(HandleUserInput);
+
+        // Προσθήκη ακροατή στο κουμπί "save" για να ανιχνεύει το πάτημα του κουμπιού
+        saveButton.onClick.AddListener(SaveWeightEntry);
+    }
+
+    void HandleUserInput(string input)
+    {
+        // Εμφανίζει την εισαγωγή του χρήστη στο UI
+        displayText.text = input;
+
+        // ’λλες ενέργειες με την εισαγωγή του χρήστη
+        Debug.Log("User input: " + input);
+    }
+
+    void SaveWeightEntry()
+    {
+        // Παίρνουμε το βάρος από το πεδίο εισαγωγής
+        if (float.TryParse(userInputField.text, out float weight))
+        {
+            // Παίρνουμε τη σημερινή ημερομηνία
+            DateTime today = DateTime.Today;
+
+            // Δημιουργία μιας νέας καταχώρησης βάρους με τη σημερινή ημερομηνία
+            WeightEntry newEntry = new WeightEntry(weight, today);
+
+            // Προσθήκη της καταχώρησης στη λίστα
+            weightEntries.Add(newEntry);
+
+            // Εμφανίζουμε μόνο το βάρος
+            displayText.text = weight.ToString();
+
+            // ’λλες ενέργειες με την καταχώρηση
+            Debug.Log($"Saved weight: {weight} kg on {today.ToString("dd/MM/yyyy")}");
+        }
+        else
+        {
+            // Εμφανίζουμε μήνυμα λάθους αν η είσοδος δεν είναι έγκυρος αριθμός
+            displayText.text = "Invalid input. Please enter a valid number.";
+            Debug.Log("Invalid input. Please enter a valid number.");
+        }
+    }
+}

9698555d3e6bcc327677a384cbc168df2b538fce
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2024-05-21 16:29:26 +0300

    Added menu for exercise related questions(needs adjustment)

---
+++ Assets/Scenes/TrainerChat/QuestionHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestionHandler : MonoBehaviour
+{
+    public TMP_Text mainText;
+    public Button GeneralExercises;
+    public Button ExercisePrograms;
+    public Button ExerciseSpecific;
+    public Button InjuryPrevention;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

5ba371f9c95ccf62390e3992e4c1359629ea6f5f
chouchoufay <fay4801@gmail.com>
2024-06-02 20:31:07 +0300

    added code in myProgress meal

--- Assets/Scenes/MyProgress/dinnerTracker.cs
+++ Assets/Scenes/MyProgress/dinnerTracker.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class BreakfastEntry : MonoBehaviour
+public class DinnerEntry : MonoBehaviour
 {
     public TMP_InputField foodNameInput;
     public TMP_InputField caloriesInput;
@@ -12,9 +12,9 @@ public class BreakfastEntry : MonoBehaviour
     void Start()
     {
         // Έλεγχος αν υπάρχουν ήδη αποθηκευμένα δεδομένα για την τρέχουσα ημερομηνία
-        string savedDate = PlayerPrefs.GetString("BreakfastEntryDate", "");
+        string savedDate = PlayerPrefs.GetString("DinnerEntryDate", "");
 
-        if (savedDate == DateTime.Today.ToString())
+        if (savedDate == DateTime.Today.ToString("d"))
         {
             // Αν υπάρχουν δεδομένα για την τρέχουσα ημερομηνία, απενεργοποιούμε τα πεδία εισόδου και το κουμπί
             foodNameInput.interactable = false;
@@ -22,16 +22,16 @@ public class BreakfastEntry : MonoBehaviour
             saveButton.gameObject.SetActive(false);
 
             // Εμφάνιση των αποθηκευμένων δεδομένων στα πεδία εισόδου
-            string savedFoodName = PlayerPrefs.GetString("BreakfastFoodName", "No data");
-            float savedCalories = PlayerPrefs.GetFloat("BreakfastCalories", 0f);
+            string savedFoodName = PlayerPrefs.GetString("DinnerFoodName", "No data");
+            float savedCalories = PlayerPrefs.GetFloat("DinnerCalories", 0f);
             foodNameInput.text = savedFoodName;
             caloriesInput.text = savedCalories.ToString();
 
             // Εκτύπωση των αποθηκευμένων δεδομένων στην κονσόλα για επιβεβαίωση
             Debug.Log("Already saved data for today:");
-            Debug.Log("Breakfast Food Name: " + savedFoodName);
-            Debug.Log("Breakfast Calories: " + savedCalories);
-            Debug.Log("Breakfast Entry Date: " + savedDate);
+            Debug.Log("Dinner Food Name: " + savedFoodName);
+            Debug.Log("Dinner Calories: " + savedCalories);
+            Debug.Log("Dinner Entry Date: " + savedDate);
         }
         else
         {
@@ -46,15 +46,15 @@ public class BreakfastEntry : MonoBehaviour
         string foodName = foodNameInput.text;
         float calories = float.Parse(caloriesInput.text);
 
-        // Αποθηκεύουμε τα δεδομένα για το πρωινό μαζί με την σημερινή ημερομηνία
-        PlayerPrefs.SetString("BreakfastFoodName", foodName);
-        PlayerPrefs.SetFloat("BreakfastCalories", calories);
-        PlayerPrefs.SetString("BreakfastEntryDate", DateTime.Today.ToString());
+        // Αποθηκεύουμε τα δεδομένα για το βραδινό μαζί με την σημερινή ημερομηνία
+        PlayerPrefs.SetString("DinnerFoodName", foodName);
+        PlayerPrefs.SetFloat("DinnerCalories", calories);
+        PlayerPrefs.SetString("DinnerEntryDate", DateTime.Today.ToString("d"));
 
         // Εκτύπωση των δεδομένων στην κονσόλα
-        Debug.Log("Breakfast Food Name: " + foodName);
-        Debug.Log("Breakfast Calories: " + calories);
-        Debug.Log("Breakfast Entry Date: " + DateTime.Today.ToString());
+        Debug.Log("Dinner Food Name: " + foodName);
+        Debug.Log("Dinner Calories: " + calories);
+        Debug.Log("Dinner Entry Date: " + DateTime.Today.ToString("d"));
 
         // Απενεργοποίηση των πεδίων εισόδου και του κουμπιού
         foodNameInput.interactable = false;

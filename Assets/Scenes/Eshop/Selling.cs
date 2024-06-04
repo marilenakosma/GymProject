7be1058c4f6542fecea9592c8bb89b0162a9b81c
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2024-06-04 10:16:48 +0300

    Timer button changes + eshop code

---
+++ Assets/Scenes/Eshop/Selling.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class Selling : MonoBehaviour
+{
+    public TMP_InputField NameInputField;
+    public TMP_InputField CardNumberInputField;
+    public TMP_InputField ExpirationDateInputField;
+    public TMP_InputField CVVInputField;
+
+    public TMP_Text errorMessageText;
+    public TMP_Text successMessageText;
+
+    public void OnSubmitButtonClicked()
+    {
+        // Clear previous messages
+        errorMessageText.text = "";
+        successMessageText.text = "";
+
+        // Get the values from input fields
+        string Name = NameInputField.text;
+        string CardNumber = CardNumberInputField.text;
+        string ExpDate = ExpirationDateInputField.text;
+        string CVV =CVVInputField.text;
+
+        // Check for empty fields
+        if (string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(CardNumber) || string.IsNullOrWhiteSpace(ExpDate) || string.IsNullOrWhiteSpace(CVV))
+        {
+            errorMessageText.text = "Please fill out all fields.";
+        }
+        else
+        {
+            successMessageText.text = "Thank you for shopping with us!";
+        }
+    }
+}

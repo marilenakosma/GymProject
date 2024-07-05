124618d569088cc4a94bd9f5aaa04e0d0af6f660
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2024-07-05 09:45:24 +0300

    Fixed stupid button colors

--- Assets/Scenes/Eshop/SwitchToggle.cs
+++ Assets/Scenes/Eshop/SwitchToggle.cs
@@ -5,48 +5,54 @@ using UnityEngine.UI;
 
 public class SwitchToggle : MonoBehaviour
 {
-  [SerializeField] RectTransform UIHandleRectTransform;
-  [SerializeField] Color BackgroundActiveColor;
-  [SerializeField] Color HandleActiveColor;
+    [SerializeField] RectTransform UIHandleRectTransform;
 
     Image backgroundImage, handleImage;
 
-   Color BackgroundDefaultColor, handleDefaultColor;
-   Toggle switchtoggle;
-   Vector2 handlePosition;
+    Toggle switchtoggle;
+    Vector2 handlePosition;
 
-   void Awake() {
-    switchtoggle = GetComponent <Toggle> ();
+    void Awake()
+    {
+        switchtoggle = GetComponent<Toggle>();
 
-    handlePosition = UIHandleRectTransform.anchoredPosition;
-    backgroundImage = UIHandleRectTransform.parent.GetComponent <Image> ();
-    handleImage = UIHandleRectTransform. GetComponent <Image> ();
-    
-    BackgroundDefaultColor = backgroundImage.color;
-    handleDefaultColor = handleImage.color;
-    
-    switchtoggle.onValueChanged.AddListener(OnSwitch);
+        handlePosition = UIHandleRectTransform.anchoredPosition;
+        backgroundImage = UIHandleRectTransform.parent.GetComponent<Image>();
+        handleImage = UIHandleRectTransform.GetComponent<Image>();
 
-    if(switchtoggle.isOn)
-     OnSwitch(true);
-   }
+        switchtoggle.onValueChanged.AddListener(OnSwitch);
 
-   void OnSwitch(bool on) {
+        if (switchtoggle.isOn)
+            OnSwitch(true);
+    }
+
+    void OnSwitch(bool on)
+    {
+        Debug.Log($"Switch is {(on ? "On" : "Off")}");
+        Debug.Log($"Initial Handle Position: {handlePosition}");
+
+        
         if (on)
         {
             UIHandleRectTransform.anchoredPosition = handlePosition * -1;
-            backgroundImage.color= BackgroundActiveColor;
-            handleImage.color = HandleActiveColor;
+            backgroundImage.color = Color.cyan;
+            handleImage.color = Color.white;
         }
         else
         {
             UIHandleRectTransform.anchoredPosition = handlePosition;
-            backgroundImage.color= BackgroundDefaultColor;
-            handleImage.color = handleDefaultColor;
+            backgroundImage.color = Color.white;
+            handleImage.color = Color.cyan;
         }
-   }
+        
+        Debug.Log($"Updated Handle Position: {UIHandleRectTransform.anchoredPosition}");
+    }
 
-   void OnDestroy() {
-       // switchtoggle.onValueChanged.RemoveListener(OnSwitch);
-   }
+    void OnDestroy()
+    {
+        switchtoggle.onValueChanged.RemoveListener(OnSwitch);
+    }
 }
+
+
+
